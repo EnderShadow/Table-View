91de87989864c6a233a95584abcea6fe2c08b458
EnderShadow <tonkamatt98@gmail.com>
2016-02-05 10:55:23 -0600

    Part 2 Stretch 1

--- Table View/DetailViewController.swift
+++ Table View/DetailViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SafariServices
 
-class DetailViewController: UIViewController, SFSafariViewControllerDelegate
+class DetailViewController: UIViewController, SFSafariViewControllerDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate
 {
     @IBOutlet weak var collegeNameField: UITextField!
     @IBOutlet weak var collegeLocationField: UITextField!
@@ -17,11 +17,13 @@ class DetailViewController: UIViewController, SFSafariViewControllerDelegate
     @IBOutlet weak var collegeWebsiteField: UITextField!
     @IBOutlet weak var collegeImageView: UIImageView!
     
+    let imagePicker = UIImagePickerController()
     var college : College!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        imagePicker.delegate = self
         collegeNameField.text = college.name
         collegeLocationField.text = college.location
         collegePopulationField.text = college.numStudents
@@ -48,5 +50,18 @@ class DetailViewController: UIViewController, SFSafariViewControllerDelegate
     func safariViewControllerDidFinish(controller: SFSafariViewController)
     {
         controller.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func onImageTapped(sender: UITapGestureRecognizer)
+    {
+        imagePicker.sourceType = .PhotoLibrary
+        presentViewController(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        imagePicker.dismissViewControllerAnimated(true) { () -> Void in
+            self.collegeImageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        }
     }
 }
